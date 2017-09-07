00393457c0124e30375ea81c5768a59b744ace18
tracer-ics2017 <tracer@njuics.org>
2017-09-08 03:37:19 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  03:37:19 up 1 day,  1:11,  1 user,  load average: 0.08, 0.10, 0.09 2341fec5ee05dda7cd4c89e875f3a6c0a81c50dc

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -11,6 +11,8 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   rtl_and(&t0, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t0);
+
   rtl_update_ZFSF(&t0, id_dest->width);
   rtl_set_OF(&tzero);
   rtl_set_CF(&tzero);
