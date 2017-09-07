faa5873f9a757893f07a6fd21138be6845155ddc
tracer-ics2017 <tracer@njuics.org>
2017-09-08 00:12:48 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  00:12:48 up 21:46,  1 user,  load average: 0.07, 0.17, 0.23 553446a750f46dfd8776226d693c5f8d55052f6a

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,13 +14,13 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:  *dest = cpu.OF; break;
-    case CC_B:  *dest = cpu.CF; break;
-    case CC_E:  *dest = cpu.ZF; break;
-    case CC_BE: *dest = cpu.CF || cpu.ZF; break;
-    case CC_S:  *dest = cpu.SF; break;
-    case CC_L:  *dest = (cpu.SF != cpu.OF); break;
-    case CC_LE: *dest = cpu.ZF || (cpu.SF != cpu.OF); break;
+    case CC_O:  *dest = cpu.OF ? 1 : 0; break;
+    case CC_B:  *dest = cpu.CF ? 1 : 0; break;
+    case CC_E:  *dest = cpu.ZF ? 1 : 0; break;
+    case CC_BE: *dest = cpu.CF || cpu.ZF ? 1 : 0; break;
+    case CC_S:  *dest = cpu.SF ? 1 : 0; break;
+    case CC_L:  *dest = (cpu.SF != cpu.OF) ? 1 : 0; break;
+    case CC_LE: *dest = cpu.ZF || (cpu.SF != cpu.OF) ? 1 : 0; break;
     default: panic("should not reach here"); break;
     case CC_P: panic("n86 does not have PF"); break;
   }
