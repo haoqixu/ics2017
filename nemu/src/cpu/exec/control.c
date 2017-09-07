dd064aaf4be561bf3d7a894f4a0f14ca234d9b50
tracer-ics2017 <tracer@njuics.org>
2017-09-07 19:23:14 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  19:23:14 up 16:57,  1 user,  load average: 0.15, 0.09, 0.02 807d93e375f454cf39fb2b8f969dcae98972f11c

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,8 +25,8 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  decoding.is_jmp = 1;
   rtl_push(&decoding.seq_eip);
+  decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
 }
