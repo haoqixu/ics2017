cf36fc18e6bfddb280d2b36b8b31cbd94f5eb93d
tracer-ics2017 <tracer@njuics.org>
2017-09-10 02:16:09 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  02:16:09 up  2:01,  1 user,  load average: 0.26, 0.22, 0.22 8bbbc7276b85329443c27a663238b260b8ebda12

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -51,6 +51,7 @@ make_EHelper(xor);
 make_EHelper(sar);
 make_EHelper(shl);
 make_EHelper(shr);
+make_EHelper(rol);
 make_EHelper(test);
 
 make_EHelper(in);
--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -84,3 +84,21 @@ make_EHelper(not) {
 
   print_asm_template1(not);
 }
+
+make_EHelper(rol) {
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  if (decoding.is_operand_size_16) {
+    rtl_addi(&t1, &tzero, 16);
+  } else {
+    rtl_addi(&t1, &tzero, 32);
+  }
+  rtl_sub(&t1, &t1, &id_src->val);
+  rtl_shr(&t2, &id_dest->val, &t1);
+  rtl_or(&t0, &t0, &t1);
+  operand_write(id_dest, &t0);
+
+  // unnecessary to update CF and OF in NEMU
+  rtl_update_ZFSF(&t0, id_dest->width);
+
+  print_asm_template2(shl);
+}
