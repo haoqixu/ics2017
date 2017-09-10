8b9f772242e6a9ef76d7e461f13555f7397a155d
tracer-ics2017 <tracer@njuics.org>
2017-09-10 17:14:35 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  17:14:35 up 17:00,  1 user,  load average: 0.11, 0.16, 0.17 cbdf1d028a4356419d3f5b21a246e9b097e8c3ff

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -31,25 +31,6 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-  union {
-    GateDesc gd;
-    struct { uint32_t lo, hi; };
-  } item;
-  vaddr_t addr;
-
-  addr = 8 * id_dest->val + cpu.idtr.base;
-  item.lo = vaddr_read(addr, 4);
-  item.hi = vaddr_read(addr + 4, 4);
-
-  decoding.is_jmp = 1;
-  decoding.jmp_eip = (item.gd.offset_15_0 & 0xFFFF)
-    & ((item.gd.offset_31_16 & 0xFFFF) << 16);
-
-  t0 = cpu.cs;
-  rtl_push(&t0);
-  rtl_push(&cpu.eflags);
-  rtl_push(&decoding.seq_eip);
-
   print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,24 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  union {
+    GateDesc gd;
+    struct { uint32_t lo, hi; };
+  } item;
+  vaddr_t addr;
 
-  TODO();
+  addr = 8 * id_dest->val + cpu.idtr.base;
+  item.lo = vaddr_read(addr, 4);
+  item.hi = vaddr_read(addr + 4, 4);
+
+  decoding.is_jmp = 1;
+  decoding.jmp_eip = (item.gd.offset_15_0 & 0xFFFF)
+    & ((item.gd.offset_31_16 & 0xFFFF) << 16);
+
+  t0 = cpu.cs;
+  rtl_push(&t0);
+  rtl_push(&cpu.eflags);
+  rtl_push(&decoding.seq_eip);
 }
 
 void dev_raise_intr() {
