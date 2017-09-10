8f756cee1c011f1b83fd5a164d33a99d42b91629
U201414606-Xu Haoqi <hqcat6@gmail.com>
2017-09-10 15:24:19 +0800

    implement 'loader' function

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,14 @@
 #include "common.h"
 
+extern uint8_t ramdisk_start;
+extern uint8_t ramdisk_end;
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t nbyte = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, nbyte);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
