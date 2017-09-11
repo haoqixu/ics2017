c96baad154e6d7be4e4469c730ef2edda8fc156f
U201414606-Xu Haoqi <hqcat6@gmail.com>
2017-09-12 02:52:57 +0800

    add support of '/dev/fb' and '/proc/dispinfo'.

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,9 +15,16 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  int row, col;
+  offset /= 4;
+  col = offset % _screen.width;
+  row = offset / _screen.width;
+
+  _draw_rect(buf, col, row, len/4, 1);
 }
 
 void init_device() {
@@ -25,4 +32,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d\n", _screen.width, _screen.height);
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -2,6 +2,8 @@
 
 void ramdisk_read(void *buf, off_t offset, size_t len);
 void ramdisk_write(const void *buf, off_t offset, size_t len);
+void fb_write(const void *buf, off_t offset, size_t len);
+void dispinfo_read(void *buf, off_t offset, size_t len);
 
 typedef struct {
   char *name;
@@ -27,11 +29,17 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+  file_table[FD_FB].size = _screen.width * _screen.height * sizeof(uint32_t);
+  file_table[FD_FB].open_offset = 0;
 }
 
 ssize_t fs_write(int fd, uint8_t *buf, size_t len) {
   size_t i = 0;
+  size_t size, nwrite;
   Finfo *fp = &file_table[fd];
+
+  size = fp->size - fp->open_offset;
+  nwrite = len > size ? size : len;
 
   switch (fd) {
   case FD_STDOUT:
@@ -40,14 +48,18 @@ ssize_t fs_write(int fd, uint8_t *buf, size_t len) {
       _putc(*buf++);
     return len;
 
+  case FD_FB:
+    fb_write(buf, fp->open_offset, nwrite);
+    break;
+
   default:
     if (fd < 6 || fd >= NR_FILES)
       return -1;
-    assert(len <= fp->size - fp->open_offset);
-    ramdisk_write(buf, fp->disk_offset + fp->open_offset, len);
-    fp->open_offset += len;
-    return len;
+    ramdisk_write(buf, fp->disk_offset + fp->open_offset, nwrite);
+    break;
   }
+  fp->open_offset += nwrite;
+  return nwrite;
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
@@ -58,6 +70,9 @@ int fs_open(const char *pathname, int flags, int mode) {
       break;
   assert(fd != NR_FILES);
   file_table[fd].open_offset = 0;
+
+  Log("open [%d]%s", fd, pathname);
+
   return fd;
 }
 
@@ -65,27 +80,32 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
   ssize_t size, nread;
   Finfo *fp = &file_table[fd];
 
+  size = fp->size - fp->open_offset;
+  nread = len > size ? size : len;
+
   switch (fd) {
   case FD_STDOUT:
   case FD_STDERR:
     return -1;
 
+  case FD_DISPINFO:
+    dispinfo_read(buf, fp->open_offset, len);
+    break;
+
   default:
     if (fd < 6 || fd >= NR_FILES)
       return -1;
-    size = fp->size - fp->open_offset;
-    nread = len > size ? size : len;
     ramdisk_read(buf, fp->disk_offset + fp->open_offset, nread);
-    fp->open_offset += nread;
-    return nread;
+    break;
   }
+  fp->open_offset += nread;
+  return nread;
 }
 
 off_t fs_lseek(int fd, off_t offset,int whence) {
   Finfo *fp = &file_table[fd];
 
-  if (fd < 6 || fd > NR_FILES)
-    return 0;
+  assert(fd < NR_FILES);
 
   switch (whence) {
   case SEEK_END: offset = fp->size + offset; break;
