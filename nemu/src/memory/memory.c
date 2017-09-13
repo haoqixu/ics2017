86b6c79fa70488c3fcb00dc3f0130c8667384446
tracer-ics2017 <tracer@njuics.org>
2017-09-14 03:17:03 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  03:17:03 up 4 days,  3:02,  1 user,  load average: 0.56, 0.34, 0.32 7e3e7fbdd959956a5aa14d6a3a3f128a90b22048

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -61,7 +61,6 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
   if (CROSS_PAGE(addr)) {
     /* data cross the page boundary */
     Log("vaddr_read cross page boundary: 0x%x len:%d", addr, len);
-    assert(0);
     union {
       uint8_t bytes[4];
       uint32_t dword;
