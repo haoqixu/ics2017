4c23b33e5e2a0b1d8c57d20d4c7ce0ddb5a6d589
tracer-ics2017 <tracer@njuics.org>
2017-09-14 02:00:48 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  02:00:48 up 4 days,  1:46,  1 user,  load average: 0.29, 0.40, 0.35 6d5f5cf0ccd2a16775329825aba69b6d5ab21812

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -36,24 +36,24 @@ paddr_t page_translate(vaddr_t addr, bool is_write) {
 
   if (cpu.cr0.paging) {
     pgdir = (PDE *)(intptr_t)(cpu.cr3.page_directory_base << 12);
-    pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & 0x3ff], 4);
+    pde.val = paddr_read((intptr_t)&pgdir[(addr >> 22) & PAGE_MASK], 4);
     assert(pde.present);
     pde.accessed = 1;
 
     pgtab = (PTE *)(intptr_t)(pde.page_frame << 12);
-    pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & 0x3ff], 4);
+    pte.val = paddr_read((intptr_t)&pgtab[(addr >> 12) & PAGE_MASK], 4);
     assert(pte.present);
     pte.accessed = 1;
     pte.dirty = is_write ? 1 : pte.dirty;
 
-    paddr = (pte.page_frame << 12) | (addr & 0x3ff);
+    paddr = (pte.page_frame << 12) | (addr & PAGE_MASK);
   }
 
   return paddr;
 }
 
 #define CROSS_PAGE(addr) \
-  ((((addr) + len) & PAGE_MASK) != ((addr) & PAGE_MASK))
+  ((((addr) + len) & ~PAGE_MASK) != ((addr) & ~PAGE_MASK))
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
   if (CROSS_PAGE(addr)) {
