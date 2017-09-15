ffcd86dca7432bc51ce93dcad02fa7531cf8455d
tracer-ics2017 <tracer@njuics.org>
2017-09-16 04:34:06 +0800

    >  compile U201414606 xuxu Linux arch 4.12.13-1-ARCH #1 SMP PREEMPT Fri Sep 15 06:36:43 UTC 2017 x86_64 GNU/Linux  04:34:06 up  2:14,  1 user,  load average: 1.24, 1.29, 1.17 3770a5c8f314915244420eac39f1f4bdbff9d47a

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -70,8 +70,18 @@ make_EHelper(shr) {
 }
 
 make_EHelper(shrd) {
-  rtl_shr(&t0, &id_src2->val, &id_src->val);
+  rtl_shr(&t0, &id_dest->val, &id_src->val);
+  if (decoding.is_operand_size_16) {
+    rtl_addi(&t1, &tzero, 16);
+  } else {
+    rtl_addi(&t1, &tzero, 32);
+  }
+  rtl_sub(&t1, &t1, &id_src->val);
+  rtl_shl(&t2, &id_src2->val, &t1);
+
+  rtl_or(&t0, &t0, &t2);
   operand_write(id_dest, &t0);
+
   // unnecessary to update CF and OF in NEMU
   rtl_update_ZFSF(&t0, id_dest->width);
 
@@ -79,8 +89,18 @@ make_EHelper(shrd) {
 }
 
 make_EHelper(shld) {
-  rtl_shl(&t0, &id_src2->val, &id_src->val);
+  rtl_shl(&t0, &id_dest->val, &id_src->val);
+  if (decoding.is_operand_size_16) {
+    rtl_addi(&t1, &tzero, 16);
+  } else {
+    rtl_addi(&t1, &tzero, 32);
+  }
+  rtl_sub(&t1, &t1, &id_src->val);
+  rtl_shr(&t2, &id_src2->val, &t1);
+
+  rtl_or(&t0, &t0, &t2);
   operand_write(id_dest, &t0);
+
   // unnecessary to update CF and OF in NEMU
   rtl_update_ZFSF(&t0, id_dest->width);
 
