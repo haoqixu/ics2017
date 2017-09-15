64b9e0f94545e1963e6b27f6f35a44642524207b
tracer-ics2017 <tracer@njuics.org>
2017-09-15 20:51:50 +0800

    >  compile U201414606 xuxu Linux arch 4.12.8-2-ARCH #1 SMP PREEMPT Fri Aug 18 14:08:02 UTC 2017 x86_64 GNU/Linux  20:51:50 up 5 days, 20:37,  1 user,  load average: 0.08, 0.09, 0.06 d4eef38cf9c588c1a955fa84fcde8912bcf92022

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -248,7 +248,7 @@ void exec_wrapper(bool print_flag) {
   
   #define TIMER_IRQ 32
   extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
-  if (cpu.INTR & cpu.IF) {
+  if (cpu.INTR && cpu.IF) {
     cpu.INTR = false;
     raise_intr(TIMER_IRQ, cpu.eip);
   }
